b7cf97450f5e62f6d0dfd3997cd3fc53bf8a8d6e
JolliestJames <martinja@oregonstate.edu>
2018-08-05 16:16:16 -0700

    Implement gigasecond::after()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 extern crate chrono;
-use chrono::{DateTime, Utc};
+
+use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let one_billion = 10_i64.pow(9);
+    let one_billion_seconds = Duration::seconds(one_billion);
+    let one_billion_seconds_after_start: DateTime<Utc> = start + one_billion_seconds;
+
+    return one_billion_seconds_after_start;
 }
