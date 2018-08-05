874ce0ebc4e231f981536b3086226b0b7f1d1292
JolliestJames <martinja@oregonstate.edu>
2018-08-05 16:35:41 -0700

    Refactor gigasecond::after() implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,10 @@ extern crate chrono;
 
 use chrono::{DateTime, Utc, Duration};
 
+const ONE_BILLION: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let one_billion = 10_i64.pow(9);
-    let one_billion_seconds = Duration::seconds(one_billion);
-    let one_billion_seconds_after_start: DateTime<Utc> = start + one_billion_seconds;
-
-    return one_billion_seconds_after_start;
+    let one_billion_seconds = Duration::seconds(ONE_BILLION);
+    start + one_billion_seconds
 }
